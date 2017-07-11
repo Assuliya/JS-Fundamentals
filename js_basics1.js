8eb2223eb0ea0d1e023cbb6ae32a0d0e62603ea0
Assuliya <t.nesterchuk@mail.ru>
2017-07-11 22:28:20 +0300

    coding dojo. JS basics 1 assignment

---
+++ js_basics1.js
@@ -0,0 +1,46 @@
+//Basic 1
+
+// var x = [];
+// x.push('coding', 'dojo', 'rocks')
+// x.pop()
+// console.log(x)
+
+
+//Basic 2
+
+// const y = [];
+// console.log(y)
+// y.push(88)
+// console.log(y)
+
+
+// Basic 3
+
+// var x = [9, 10, 6, 5, -1, 20, 13, 2];
+
+// for(var i = 0; i<x.length; i++){
+// 	if(i + 1 == x.length){
+// 		break;
+// 	}
+// 	console.log(x[i])
+// }
+
+
+// Basic 4
+
+// var names = ['Kadie', 'Joe', 'Fritz', 'Pierre', 'Alphonso'];
+
+// for(var i = 0; i < names.length; i++){
+// 	console.log(names[i].length)
+// }
+
+
+// Basic 5
+
+function yell(string){
+	return string.toUpperCase()
+}
+
+console.log(yell('hi my name is ...'))
+
+
